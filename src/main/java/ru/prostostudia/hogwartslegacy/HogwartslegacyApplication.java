50920619bca5566b94991ef12f6d21e25f242ae0
RomanKuzmin <ghelatin@mail.ru>
2024-10-26 14:26:01 +0300

    init commit

---
+++ src/main/java/ru/prostostudia/hogwartslegacy/HogwartslegacyApplication.java
@@ -0,0 +1,14 @@
+package ru.prostostudia.hogwartslegacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartslegacyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HogwartslegacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/hogwartslegacy/HogwartslegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.hogwartslegacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartslegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
