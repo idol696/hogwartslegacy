caa961841f84f5c2591aaf1f80208e739cef97b2
Roman Kuzmin <ghelatin@mail.ru>
2024-12-20 17:07:53 +0300

    HomeWork Master Fix 2

--- src/test/java/ru/prostostudia/hogwartslegacy/HogwartsLegacyApplicationTests.java
+++ src/test/java/ru/prostostudia/hogwartslegacy/HogwartsLegacyApplicationTests.java
@@ -3,6 +3,7 @@ package ru.prostostudia.hogwartslegacy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import ru.prostostudia.hogwartslegacy.controllers.FacultyController;
 import ru.prostostudia.hogwartslegacy.controllers.StudentController;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
